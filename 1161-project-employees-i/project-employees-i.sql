8b65a1af44988825408735b6086b4e4633ee49eb
vansh7574 <37745628+vansh7574@users.noreply.github.com>
2026-01-06 18:33:11 -0500

    Time: 1078 ms (81.87%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1161-project-employees-i/project-employees-i.sql
@@ -0,0 +1,2 @@
+/* Write your T-SQL query statement below */
+select p.project_id, ROUND(AVG(experience_years * 1.0), 2) as average_years from Project p Left Join Employee e on p.employee_id = e.employee_id group by p.project_id
