d408715042c8947c236b5d26ba53210286d91979
vansh7574 <37745628+vansh7574@users.noreply.github.com>
2026-01-05 21:49:08 -0500

    Time: 425 ms (80.29%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -6,11 +6,11 @@ with confirmed as
  select s.user_id, COUNT(case when c.action = 'confirmed' THEN 1 END) over (partition by s.user_id) as confirmed_counts, COUNT(c.user_id) over (partition by c.user_id) as all_requests
  from Signups s Left Join Confirmations c on s.user_id = c.user_id
 
-),
-confirmed_result as (
-   Select distinct user_id, COALESCE(ROUND(confirmed_counts * 1.0 / Nullif(all_requests,0), 2), 0) as confirmation_rate from confirmed
 )
-Select * from confirmed_result 
+
+Select distinct user_id, COALESCE(ROUND(confirmed_counts * 1.0 / Nullif(all_requests,0), 2), 0) as confirmation_rate from confirmed
+
+-- Select * from confirmed_result 
 
 -- Select Distinct * from confirmed
 
