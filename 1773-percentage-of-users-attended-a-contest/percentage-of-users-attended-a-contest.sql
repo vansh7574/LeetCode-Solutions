05fc17e0e0e104b9483266ccb19f3a4334c955c6
vansh7574 <37745628+vansh7574@users.noreply.github.com>
2026-01-07 11:55:16 -0500

    Time: 2763 ms (37.10%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,2 @@
+/* Write your T-SQL query statement below */
+select contest_id, Round(100.0 * Count(*)/(Select count(*) from Users),2) as percentage from Register group by contest_id order by percentage desc, contest_id ASC
