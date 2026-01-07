6a5f7a0014a25f6c84a6b7b0938b51d6fafffd46
vansh7574 <37745628+vansh7574@users.noreply.github.com>
2026-01-06 20:49:43 -0500

    Time: 356 ms (30.14%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,13 +1,14 @@
 /* Write your T-SQL query statement below */
 -- with totals as (
---     select p.product_id, (p.price * Nullif(u.units, 0)) as total_amount, Sum(NullIf(units,0)) over (partition by p.product_id) as total_units from Prices p Left Join UnitsSold u on p.product_id = u.product_id where u.purchase_date BETWEEN start_date and end_date
+--     select p.product_id, (p.price * isNull(u.units, 0)) as total_amount, Sum(isNull(units,0)) over (partition by p.product_id) as total_units from Prices p Left Join UnitsSold u on p.product_id = u.product_id where u.purchase_date BETWEEN start_date and end_date
 -- ),
 -- product_totals as (
 --      select distinct product_id, sum(total_amount) over (partition by product_id) as total_per_product, total_units from totals
 -- )
 
 -- Select product_id, ROUND(total_per_product * 1.0/total_units,2) as average_price from product_totals
--- Select product_id, Sum(total_amount * 1.0) OVER (partition by product_id)/ Sum(total_units) over (partition by product_id)  as average_price  from totals
+
+
 
 SELECT
     p.product_id,
@@ -24,4 +25,3 @@ LEFT JOIN UnitsSold AS u
    AND u.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY
     p.product_id;
-
