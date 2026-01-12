5e5105469b0422358869bffb00811e34048ca3c2
vansh7574 <37745628+vansh7574@users.noreply.github.com>
2026-01-12 10:19:20 -0500

    Time: 1210 ms (9.31%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,2 @@
+/* Write your T-SQL query statement below */
+Select activity_date as day, Count(distinct user_id) as active_users from Activity where activity_date >= '2019-06-28' and activity_date <= '2019-07-27' group by activity_date order by day
