630bee629a3c81b8871e32f81e5e67ea694d330c
vansh7574 <37745628+vansh7574@users.noreply.github.com>
2026-01-13 13:56:05 -0500

    Time: 1042 ms (5.09%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,6 +1,10 @@
 /* Write your T-SQL query statement below */
-with num_departments as (
-    Select employee_id, count(*) as num_depts from Employee group by employee_id
-)
-
-Select e.employee_id, e.department_id from Employee e Left Join num_departments n on e.employee_id = n.employee_id where n.num_depts = 1 or e.primary_flag = 'Y'
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+   OR employee_id IN (
+        SELECT employee_id
+        FROM Employee
+        GROUP BY employee_id
+        HAVING COUNT(*) = 1
+   );
