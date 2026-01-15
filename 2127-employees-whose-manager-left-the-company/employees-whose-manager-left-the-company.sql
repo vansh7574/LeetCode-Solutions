199e4f1799b59683712ee5616fa30721d2c1d297
vansh7574 <37745628+vansh7574@users.noreply.github.com>
2026-01-15 14:42:41 -0500

    Time: 377 ms (61.34%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -0,0 +1,4 @@
+/* Write your T-SQL query statement below */
+Select employee_id from Employees where salary < 30000 and manager_id not in (
+    select employee_id from Employees
+) order by employee_id
