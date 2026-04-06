9e0c8826dd63cca7566e3c3883b1b4cdebb5b949
vansh7574 <37745628+vansh7574@users.noreply.github.com>
2026-04-06 12:10:59 -0400

    Time: 355 ms (27.67%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3767-find-students-who-improved/find-students-who-improved.sql
@@ -0,0 +1,12 @@
+/* Write your T-SQL query statement below */
+-- # Write your MySQL query statement below
+-- #select l1.student_id, l1.subject, l1.score as first_score, l2.score as latest_score from latest_score l1 inner join latest_score l2 on l1.rn = 1 and l2.rn = l2.latest where l1.score < l2.score order by student_id, subject
+
+with cte as (
+    select student_id, subject, score, exam_date, Row_Number() over (partition by student_id, subject order by exam_date) as rn from Scores
+),
+latest_scores as (
+    select student_id, subject, score, exam_date, rn, max(rn) over (partition by student_id, subject) as latest from cte  
+)
+
+select l1.student_id, l1.subject, l1.score as first_Score, l2.score as latest_score from latest_scores l1 inner join latest_scores l2 on l1.student_id = l2.student_id and l1.subject = l2.subject and l1.rn = 1 and l2.rn = l2.latest and l1.rn < l2.rn where l1.score < l2.score order by l1.student_id, l1.subject
