f118ad8eb3bcacb9e7509ed5d65758b15d9ddb80
vansh7574 <37745628+vansh7574@users.noreply.github.com>
2026-04-08 12:42:14 -0400

    Time: 478 ms (27.07%) | Memory: 0B (100.00%) - LeetSync

--- 3848-analyze-subscription-conversion/analyze-subscription-conversion.sql
+++ 3848-analyze-subscription-conversion/analyze-subscription-conversion.sql
@@ -1,8 +1,12 @@
 /* Write your T-SQL query statement below */
 
+-- with CTE as (
+--     Select distinct user_id, Round(Avg(case when activity_type = 'free_trial' Then activity_duration * 1.0 END) over (partition by user_id), 2)   as trial_avg_duration, Round(Avg(case when activity_type = 'paid' Then activity_duration * 1.0 END) over (partition by user_id ), 2)  as paid_avg_duration from UserActivity 
+-- )
+
+-- Select * from cte where paid_avg_duration is not null
 with CTE as (
-    Select distinct user_id, Round(Avg(case when activity_type = 'free_trial' Then activity_duration * 1.0 END) over (partition by user_id), 2)   as trial_avg_duration, Round(Avg(case when activity_type = 'paid' Then activity_duration * 1.0 END) over (partition by user_id ), 2)  as paid_avg_duration from UserActivity 
+    Select user_id, round(avg(case when activity_type = 'free_trial' Then activity_duration * 1.0 END),2) as trial_avg_duration, round(avg(case when activity_type = 'paid' Then activity_duration * 1.0 END),2) as paid_avg_duration from UserActivity group by user_id 
 )
 
-Select * from cte where paid_avg_duration is not null
--- Select user_id, avg(case when activity_type = 'free_trial' Then activity_duration END) as trail_avg_duration
+select * from CTE where paid_avg_duration is not null
