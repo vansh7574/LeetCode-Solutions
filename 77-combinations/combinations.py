9acc934c4944e62e862e8555acf8f24f274ab05b
vansh7574 <37745628+vansh7574@users.noreply.github.com>
2026-05-20 16:45:51 -0400

    Time: 191 ms (7.19%) | Memory: 61.5 MB (26.63%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -1,16 +1,29 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        
         res = []
-        def backtrack(curr_k, comb, start):
-            if curr_k == 0:
-                res.append(list(comb)) 
+        if n == k:
+            temp = [i for i in range(1,n+1)]
+            res.append(temp)
+            return res
+            
+        def dfs(i, curr_size, curr_comb):
+            
+            if i > n+1:
+                return
+            if curr_size > k:
+                return
+            if curr_size == k:
+                res.append(curr_comb.copy())
                 return
 
-            for i in range(start, n+1):
-                comb.append(i)
-                backtrack(curr_k - 1, comb, i + 1)
-                comb.pop(-1)
+            curr_comb.append(i)
+            #for num in range(i+1, n+1):
+            dfs(i+1, curr_size + 1, curr_comb)
+            curr_comb.pop()
+            dfs(i+1, curr_size, curr_comb)              
 
-        backtrack(k, [], 1)
-        return res
+        dfs(1,0,[])
+        return res        
+            
+
+
